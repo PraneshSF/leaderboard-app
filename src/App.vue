71cba81629d4acefb17fbf0c7af18fa4c144ec1d
PraneshSF <pranesh.srinivasan@springfinancial.ca>
2025-04-01 12:57:01 +0530

    dark theme and title update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Leaderboard from "./components/Leaderboard.vue";
 </script>
 
 <template>
-  <div class="flex flex-col w-full">
+  <div class="flex flex-col w-full dark:text-black">
     <Leaderboard />
   </div>
 </template>
